db3c1469a46099bdae37f040325b0bc47c5f88c9
yyurimelo <yuri.devsy@gmail.com>
2026-02-28 11:24:59 -0300

    feat: add cascade and orphanRemoval in product and raw material

--- back/src/main/java/dev/java/Autoflex/model/Product.java
+++ back/src/main/java/dev/java/Autoflex/model/Product.java
@@ -3,6 +3,7 @@ package dev.java.Autoflex.model;
 import java.math.BigDecimal;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -25,33 +26,46 @@ public class Product {
     @Column(nullable = false)
     private BigDecimal price;
 
-    @OneToMany(mappedBy = "product")
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ProductRawMaterial> productRawMaterials;
 
-    public Product() {}
+    public Product() {
+    }
 
     public Product(Long id, String name, BigDecimal price) {
         this.id = id;
         this.name = name;
         this.price = price;
     }
-    
+
     public Long getId() {
         return id;
     }
+
     public String getName() {
         return name;
     }
+
     public BigDecimal getPrice() {
         return price;
     }
-    
+
     public void setId(Long id) {
         this.id = id;
     }
+
+    public List<ProductRawMaterial> getProductRawMaterials() {
+        return productRawMaterials;
+    }
+
+    public void setProductRawMaterials(List<ProductRawMaterial> productRawMaterials) {
+        this.productRawMaterials = productRawMaterials;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public void setPrice(BigDecimal price) {
         this.price = price;
     }
--- back/src/main/java/dev/java/Autoflex/model/RawMaterial.java
+++ back/src/main/java/dev/java/Autoflex/model/RawMaterial.java
@@ -1,10 +1,14 @@
 package dev.java.Autoflex.model;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,31 +25,47 @@ public class RawMaterial {
     @Column(name = "stock_quantity", nullable = false)
     private Integer stockQuantity;
 
-    public RawMaterial() {}
+    @OneToMany(mappedBy = "rawMaterial", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ProductRawMaterial> productRawMaterials;
+
+    public RawMaterial() {
+    }
 
     public RawMaterial(Long id, String name, Integer stockQuantity) {
         this.id = id;
         this.name = name;
         this.stockQuantity = stockQuantity;
     }
-    
+
     public Long getId() {
         return id;
     }
+
     public String getName() {
         return name;
     }
+
     public Integer getStockQuantity() {
         return stockQuantity;
     }
-    
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public void setStockQuantity(Integer stockQuantity) {
         this.stockQuantity = stockQuantity;
+    }
+
+    public List<ProductRawMaterial> getProductRawMaterials() {
+        return productRawMaterials;
+    }
+
+    public void setProductRawMaterials(List<ProductRawMaterial> productRawMaterials) {
+        this.productRawMaterials = productRawMaterials;
     }
 }
