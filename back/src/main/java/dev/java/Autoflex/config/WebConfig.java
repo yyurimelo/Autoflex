5349a224febdaccc3ddadcd0752e8c89a980e8e6
yuri <126303592+yyurimelo@users.noreply.github.com>
2026-02-28 23:56:30 -0300

    Update allowed origins in CORS configuration

--- back/src/main/java/dev/java/Autoflex/config/WebConfig.java
+++ back/src/main/java/dev/java/Autoflex/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/api/**")
-				.allowedOrigins("http://localhost:5173", "https://autoflex-iota.vercel.app")
+				.allowedOrigins("http://localhost:5173", "https://autoflex-lyart.vercel.app")
 				.allowedMethods("*")
 				.allowedHeaders("*")
 				.allowCredentials(true);
